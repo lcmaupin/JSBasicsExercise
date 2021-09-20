7929cf0cd5e7f2bc1ce448fa84563806dda474d7
Luke Maupin <lukecmaupin@gmail.com>
2021-09-20 19:56:07 -0400

    Creates script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log(7);
+
+//Numbers, strings, booleans, null, and undefined
+
+//+ - * / ** %
+
+console.log(5%2);
+console.log(11%5);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log('Hawk' + 'eye');
+console.log('Luke'.length + 'Maupin'.length);
+console.log('Avengers: Endgame'[16]);
+
+//Bonus
+console.log('\\');
+console.log(5**4-9);
+console.log('I'.length - 'AM'.length + 'IRON'.length - 'MAN'.length);
+console.log('CAP'[1]);
+console.log('VISION'[0]);
+console.log('OKOYE'[4]);
+console.log('NICK FURY'[0]);
+console.log('GROOT'[0]);
+console.log('DOCTOR STRANGE'[13]);
+console.log('THOR'[3]);
+console.log('STAR-LORD'[0]);
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
+
